9970e5f18fc38d2ad03e0adf2cc4d2618a77b642
goulchen <36884376+goulchen@users.noreply.github.com>
2022-09-26 23:12:51 +0200

    commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # wake_on_lan_expressjs
 
 A small express js server hosting an API to wake up a computer in the nework 
+
+## create a certificate and key with openssl
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
+
--- app.js
+++ app.js
@@ -1,47 +1,40 @@
 const express = require("express");
-const wol = require("wakeonlan");
+const wol = require("./wakeonlan");
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
 
 const port = 3000;
 
-var privateKey  = fs.readFileSync(__dirname + '/key.pem', 'utf8');
+var privateKey = fs.readFileSync(__dirname + '/key.pem', 'utf8');
 var certificate = fs.readFileSync(__dirname + '/cert.pem', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
-
-
-
+var credentials = { key: privateKey, cert: certificate };
 
 var app = express();
+console.log('sarting listening')
 
 app.get("/", (req, res) => {
-  try {
-    const secrets = JSON.parse(fs.readFileSync(__dirname + "secrets.json", 'utf8'))
-    token = secrets.token;
-  } catch (error) {
-    console.error(error);
-    console.log("couldn't get token");
-    res.status(403).end();
-  }
-  const header = req.headers;
-  if (header["token"] !== token) {
-    //res.send("wrong token!");
-    res.status(403).end();
-
-  } else {
-      console.log("sending wol")
-    wol("18:C0:4D:97:70:60", (from = "10.100.10.255")).then(() => {
-      res.send("success");
-    }).catch((error)=>console.log(error));
-  }
+    try {
+        const secrets = JSON.parse(fs.readFileSync(__dirname + "secrets.json", 'utf8'))
+        const token = secrets.token;
+        const header = req.headers;
+        if (header["token"] !== token) {
+            console.error("bad token");
+            res.status(403).end();
+        } else {
+            console.log("waking up through lan")
+            wol("18:C0:4D:97:70:60", (from = "10.100.10.255")).then(() => {
+                res.status(200).end();
+            }).catch((error) => console.log(error));
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(403).end();
+    }
 });
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
 
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(3000);
-httpsServer.listen(3001);
+httpServer.listen(port);
+httpsServer.listen(port + 1);
