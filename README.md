b60aaf6465470b9aaf1c7864aa85a3a3186b1874
goulchen <36884376+goulchen@users.noreply.github.com>
2022-09-27 02:06:32 +0200

    commit

--- README.md
+++ README.md
@@ -2,6 +2,25 @@
 
 A small express js server hosting an API to wake up a computer in the nework 
 
+this project is intended for remotely waking up a server from another computer on the same network with an open port to the internet
 ## create a certificate and key with openssl
+first install the node modules 
+npm install
+
+then you need to create a certificate to allow secure communication over the internet.
+when you create your certificate, local name should be the name of the target server (www.example.com, localhost...), otherwise curl will throw an error
+
 openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
+
+once you have generated your certificate, you can start your express server on the local machine :
+node app.js --target 18:C0:4D:97:70:60 --from 10.100.10.255 --port 3000 --token 959572f3-2250-4663-95f1-5241e1d9ba56
+
+ parameters :
+ target : the mac address of the target machine on the local network
+ from :  Source address for socket. If not specified, packets will be sent out to the broadcast address of all IPv4 interfaces.
+ port : the port for the express server, should be forwarded to an open port
+ token : an authentification token that will be asked for incoming requests
+
+
+
 
--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const wol = require("./wakeonlan");
 var fs = require('fs');
-var http = require('http');
 var https = require('https');
+const wol = require("./wakeonlan");
 
-const port = 3000;
+const target = process.argv.target || "18:C0:4D:97:70:60"
+const from = process.argv.from || "10.100.10.255"
+const port = process.argv.port || 3000;
+const token = process.argv.token || "959572f3-2250-4663-95f1-5241e1d9ba56";
 
 var privateKey = fs.readFileSync(__dirname + '/key.pem', 'utf8');
 var certificate = fs.readFileSync(__dirname + '/cert.pem', 'utf8');
@@ -14,16 +16,16 @@ var app = express();
 console.log('sarting listening')
 
 app.get("/", (req, res) => {
+    console.log("coucou")
     try {
-        const secrets = JSON.parse(fs.readFileSync(__dirname + "secrets.json", 'utf8'))
-        const token = secrets.token;
         const header = req.headers;
         if (header["token"] !== token) {
             console.error("bad token");
             res.status(403).end();
         } else {
             console.log("waking up through lan")
-            wol("18:C0:4D:97:70:60", (from = "10.100.10.255")).then(() => {
+            wol(target, (from = from)).then(() => {
+                // res.sendFile('index.html');
                 res.status(200).end();
             }).catch((error) => console.log(error));
         }
@@ -33,8 +35,9 @@ app.get("/", (req, res) => {
     }
 });
 
-var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(port);
 
-httpServer.listen(port);
-httpsServer.listen(port + 1);
+// var http = require('http');
+// var httpServer = http.createServer(app);
+// httpServer.listen(port);
